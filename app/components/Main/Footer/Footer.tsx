3b28abd749355f612d24b247699ebb9e51b4ad1c
Olga Smith <ms.olgasmith.jj@mail.ru>
2023-11-13 17:05:45 +0300

    add footer

--- app/components/Main/Footer/Footer.tsx
+++ app/components/Main/Footer/Footer.tsx
@@ -1,7 +1,11 @@
 import styles from './Footer.module.scss';
 
 const Footer = () => {
-  return <footer className={styles.footer}></footer>;
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.footer__copy}>&copy; 2023 Crypto-Sever - обменник электронных валют</p>
+    </footer>
+  );
 };
 
 export default Footer;
