c145c765fff7cdc9e4374cb37f2d0b9fb4f90339
Olga Smith <ms.olgasmith.jj@mail.ru>
2023-11-16 03:11:54 +0300

    add adaptive menu

--- app/components/Navigation/Navigation.tsx
+++ app/components/Navigation/Navigation.tsx
@@ -1,14 +1,48 @@
+'use client';
+
 import Link from 'next/link';
 import styles from './Navigation.module.scss';
 import { localBebas } from '@/public/fonts/fonts';
+import { useState } from 'react';
 
 const Navigation = () => {
+  //* menu toggle burger
+  const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false);
+  const clickOnBurgerMenu = () => {
+    setIsBurgerMenuOpen(!isBurgerMenuOpen);
+  };
+  //*close after click on nav link
+  const handleClickMenu = () => {
+    setIsBurgerMenuOpen(false);
+  };
+
   return (
-    <nav className={styles.nav}>
-        <Link href="/" style={localBebas.style} className={`${styles.nav__link} ${styles.nav__sighin} link button button_blue`}>Вход</Link>
-        <Link href="/" style={localBebas.style} className={`${styles.nav__link} ${styles.nav__reg} link button`}>Регистрация</Link>
+    <nav className={styles.menu}>
+      <div
+        className={`${isBurgerMenuOpen ? styles.burger_active : styles.burger}`}
+        onClick={clickOnBurgerMenu}></div>
+      <ul className={`${styles.nav} ${isBurgerMenuOpen ? styles.nav_active : ''}`}>
+        <li>
+          <Link
+            href="/"
+            style={localBebas.style}
+            className={`${styles.nav__link} ${styles.nav__sighin} link button button_blue`}
+            onClick={handleClickMenu}>
+            Вход
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/"
+            style={localBebas.style}
+            className={`${styles.nav__link} ${styles.nav__reg} link button`}
+            onClick={handleClickMenu}>
+            Регистрация
+          </Link>
+        </li>
+      </ul>
     </nav>
-  )
+  );
 };
 
 export default Navigation;
